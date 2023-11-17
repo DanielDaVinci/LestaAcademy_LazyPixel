309230bd55e9a82ff3f61f810287c43215966a6a
AlenMalashkin <gor2810041498@gmail.com>
2023-11-17 20:29:47 +0300

    Adding abilities abstract and dash ability

---
+++ Source/MainProject/Private/Abilities/DashAbility.cpp
@@ -0,0 +1,12 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Abilities/DashAbility.h"
+
+#include "GameFramework/Character.h"
+
+void UDashAbility::Activate()
+{
+    Character->LaunchCharacter(Character->GetVelocity() * 2000.0f, true, false);
+}
+
---
+++ Source/MainProject/Public/Abilities/DashAbility.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ActiveAbility.h"
+#include "UObject/Object.h"
+#include "DashAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAINPROJECT_API UDashAbility : public UActiveAbility
+{
+    GENERATED_BODY()
+
+    ACharacter* Character;
+    
+    virtual void Activate() override;
+};
